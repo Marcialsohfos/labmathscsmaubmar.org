4d909641f9873dca58f691ce81b75a9e404c3599
Marcialsohfos <marcialteda@gmail.com>
2026-02-02 00:24:59 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Si vos fichiers sont dans src/, Next.js les trouvera tout seul
+}
+
+module.exports = nextConfig
